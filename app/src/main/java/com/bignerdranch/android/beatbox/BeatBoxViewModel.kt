4402e1b5092625b98251a4fdb474d2ea09b11e2b
bigcoachpotato <48162523+bigcoachpotato@users.noreply.github.com>
2021-08-26 00:16:45 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxViewModel.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.beatbox
+
+import android.content.res.AssetManager
+import androidx.lifecycle.ViewModel
+
+class BeatBoxViewModel constructor(val assets: AssetManager) : ViewModel() {
+
+
+    var beatBox = BeatBox(assets)
+
+    override fun onCleared() {
+        super.onCleared()
+        beatBox.release()
+    }
+
+}
